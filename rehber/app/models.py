5e4d75a561b2107234d0dd64ee4aea28922c5512
chavdar7 <furkanchavdar@gmail.com>
2023-08-12 21:35:34 +0300

    bootstrap öncesi

--- rehber/app/models.py
+++ rehber/app/models.py
@@ -6,6 +6,8 @@ class Contact(models.Model):
     ad = models.CharField(max_length=50)
     soyad = models.CharField(max_length=50)
     telno = models.CharField(max_length=50)
+    fav = models.BooleanField(default=False)
+    block = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.ad
